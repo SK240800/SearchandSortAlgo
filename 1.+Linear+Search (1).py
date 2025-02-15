9f3c5c6113fb301294488947a568e983251b05fb
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-02-15 13:13:41 -0600

    Linear+Search .py

--- 1.+Linear+Search (1).py
+++ 1.+Linear+Search (1).py
@@ -3,12 +3,8 @@ def linear_search(arr,target):
     size = len(arr)
     for index in range(0,size):
         if(arr[index]==target):
-            return index
-    
+            return index    
     return -1
-
-
-
 my_list = [10,23,45,70,11]
 target = 700
 
